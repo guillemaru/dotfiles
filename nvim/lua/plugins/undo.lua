feebf6b8db244ee98e97ec3ac4ed411c284f52bc
Guillermo Marugan Rubio <gmarugan@vdi-cbgddp-112.dhcp.mathworks.com>
2024-08-26 10:36:00 +0100

    first commit

---
+++ nvim/lua/plugins/undo.lua
@@ -0,0 +1,11 @@
+return {
+	"mbbill/undotree",
+	keys = {
+		{ "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle [U]ndo Tree" },
+	},
+	config = function()
+		vim.g.undotree_WindowLayout = 2
+		vim.g.undotree_SetFocusWhenToggle = 1
+		-- Add more configuration options here
+	end,
+}
---
+++ nvim/lua/plugins/visuals/colorschemes/catppuccin.lua
@@ -0,0 +1,43 @@
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 2000, -- Higher than tokyonight
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+				-- miscs = {}, -- Uncomment to turn off hard-coded styles
+			},
+			color_overrides = {},
+			custom_highlights = {},
+			default_integrations = true,
+			transparent_background = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = false,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+			},
+		})
+
+		-- setup must be called before loading
+		vim.cmd.colorscheme("catppuccin")
+	end,
+}
